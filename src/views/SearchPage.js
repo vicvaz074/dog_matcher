d88f1e8b8f3f4757d68f6f650389cb9a8034f723
Vicente Vázquez <128206250+vicvaz074@users.noreply.github.com>
2023-10-11 19:38:38 -0600

    Update SearchPage.js

--- src/views/SearchPage.js
+++ src/views/SearchPage.js
@@ -3,33 +3,24 @@ import axios from 'axios';
 import { useSpring, animated } from 'react-spring';
 
 function SearchPage() {
-  // State for breed selection
   const [breeds, setBreeds] = useState([]);
   const [selectedBreed, setSelectedBreed] = useState('');
-  
-  // State for displaying dogs
   const [dogs, setDogs] = useState([]);
-  
-  // Advanced search states
   const [ageMin, setAgeMin] = useState('');
   const [ageMax, setAgeMax] = useState('');
   const [zipCode, setZipCode] = useState('');
   const [showAdvanced, setShowAdvanced] = useState(false);
-  
-  // Favorites and pagination
   const [favorites, setFavorites] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [showFavorites, setShowFavorites] = useState(false);
   const pageSize = 10;
 
-  // Animation for fade in effect
   const fadeIn = useSpring({
     opacity: 1,
     from: { opacity: 0 },
     delay: 200
   });
 
-  // Fetch the list of breeds on component mount
   useEffect(() => {
     async function fetchBreeds() {
       const response = await axios.get('https://frontend-take-home-service.fetch.com/dogs/breeds');
@@ -38,11 +29,9 @@ function SearchPage() {
     fetchBreeds();
   }, []);
 
-  // Handle search with pagination and advanced options
   const handleSearch = async (page = 1) => {
     setShowFavorites(false);
     let query = `https://frontend-take-home-service.fetch.com/dogs/search?breeds=${selectedBreed}&from=${(page - 1) * pageSize}&size=${pageSize}`;
-    
     if (showAdvanced) {
       if (ageMin) query += `&ageMin=${ageMin}`;
       if (ageMax) query += `&ageMax=${ageMax}`;
@@ -60,7 +49,6 @@ function SearchPage() {
     setCurrentPage(page);
   };
 
-  // Toggle favorite status for a dog
   const toggleFavorite = (dogId) => {
     if (favorites.includes(dogId)) {
       setFavorites(favorites.filter(id => id !== dogId));
@@ -69,21 +57,10 @@ function SearchPage() {
     }
   };
 
-  // Display only favorite dogs
   const handleViewFavorites = () => {
     setShowFavorites(true);
     const favoriteDogs = dogs.filter(dog => favorites.includes(dog.id));
     setDogs(favoriteDogs);
-  };
-
-  // Randomly match with a dog
-  const handleRandomMatch = () => {
-    if (dogs.length) {
-      const randomDog = dogs[Math.floor(Math.random() * dogs.length)];
-      alert(`Matched with: ${randomDog.name}`);
-    } else {
-      alert('No dogs available for matching!');
-    }
   };
 
   return (
@@ -110,11 +87,8 @@ function SearchPage() {
           <input type="text" id="zipCode" value={zipCode} onChange={(e) => setZipCode(e.target.value)} />
         </div>
       )}
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <button onClick={handleViewFavorites}>View Favorites</button>
-        <button onClick={handleRandomMatch} className="ml-2">Match with a Random Dog</button>
-      </div>
-      <div className="row mt-4">
+      <button className="btn btn-info mb-4" onClick={handleViewFavorites}>View Favorites</button>
+      <div className="row">
         {dogs.map(dog => (
           <div key={dog.id} className="col-md-4 mb-4">
             <div className="card">
@@ -125,19 +99,17 @@ function SearchPage() {
                 <p className="card-text">Breed: {dog.breed}</p>
                 <p className="card-text">Zip Code: {dog.zip_code}</p>
                 <button onClick={() => toggleFavorite(dog.id)}>
-                  {favorites.includes(dog.id) ? 'Remove from Favorites' : 'Add to Favorites'}
+                  {favorites.includes(dog.id) ? '💔 Remove from Favorites' : '❤️ Add to Favorites'}
                 </button>
               </div>
             </div>
           </div>
         ))}
       </div>
-      {!showFavorites && (
-        <div style={{display: 'flex', justifyContent: 'center'}}>
-          <button onClick={() => handleSearch(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
-          <button onClick={() => handleSearch(currentPage + 1)} className="ml-2">Next</button>
-        </div>
-      )}
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <button onClick={() => handleSearch(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
+        <button onClick={() => handleSearch(currentPage + 1)}>Next</button>
+      </div>
     </animated.div>
   );
 }
