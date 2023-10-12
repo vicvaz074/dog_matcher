ae59c285c7458c40a6fe0b3bd21fa21da3cbfb81
Vicente Vázquez <128206250+vicvaz074@users.noreply.github.com>
2023-10-11 19:10:46 -0600

    Update SearchPage.js

--- src/views/SearchPage.js
+++ src/views/SearchPage.js
@@ -12,7 +12,7 @@ function SearchPage() {
   const [showAdvanced, setShowAdvanced] = useState(false);
   const [favorites, setFavorites] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const pageSize = 10;
+  const [showFavorites, setShowFavorites] = useState(false);
 
   const fadeIn = useSpring({
     opacity: 1,
@@ -29,6 +29,7 @@ function SearchPage() {
   }, []);
 
   const handleSearch = async (page = 1) => {
+    setShowFavorites(false);
     let query = `https://frontend-take-home-service.fetch.com/dogs/search?breeds=${selectedBreed}&from=${(page - 1) * pageSize}&size=${pageSize}`;
     if (showAdvanced) {
       if (ageMin) query += `&ageMin=${ageMin}`;
@@ -58,49 +59,76 @@ function SearchPage() {
   return (
     <animated.div style={fadeIn} className="container mt-5">
       <h1>Search Page</h1>
-      <div className="mb-4">
-        <label htmlFor="breedSelect" className="form-label">Select a Breed</label>
-        <select id="breedSelect" className="form-select" value={selectedBreed} onChange={(e) => setSelectedBreed(e.target.value)}>
-          <option value="">Select a breed...</option>
-          {breeds.map(breed => <option key={breed} value={breed}>{breed}</option>)}
-        </select>
-      </div>
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <button className="btn btn-primary mb-4" onClick={() => handleSearch(1)}>Search</button>
-        <button className="btn btn-secondary mb-4 ml-2" onClick={() => setShowAdvanced(!showAdvanced)}>Advanced Options</button>
-      </div>
-      {showAdvanced && (
-        <div className="mb-4">
-          <label htmlFor="ageMin">Min Age:</label>
-          <input type="number" id="ageMin" value={ageMin} onChange={(e) => setAgeMin(e.target.value)} />
-          <label htmlFor="ageMax">Max Age:</label>
-          <input type="number" id="ageMax" value={ageMax} onChange={(e) => setAgeMax(e.target.value)} />
-          <label htmlFor="zipCode">Zip Code:</label>
-          <input type="text" id="zipCode" value={zipCode} onChange={(e) => setZipCode(e.target.value)} />
+      <button className="btn btn-info mb-4" onClick={() => setShowFavorites(!showFavorites)}>{showFavorites ? 'Hide' : 'Show'} Favorites</button>
+      {showFavorites ? (
+        <div className="row">
+          {favorites.map(favId => {
+            const dog = dogs.find(d => d.id === favId);
+            if (dog) {
+              return (
+                <div key={dog.id} className="col-md-4 mb-4">
+                  <div className="card">
+                    <img src={dog.img} alt={dog.name} className="card-img-top" />
+                    <div className="card-body">
+                      <h5 className="card-title">{dog.name}</h5>
+                      <p className="card-text">Age: {dog.age}</p>
+                      <p className="card-text">Breed: {dog.breed}</p>
+                      <p className="card-text">Zip Code: {dog.zip_code}</p>
+                      <button onClick={() => toggleFavorite(dog.id)}>
+                        Remove from Favorites
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
         </div>
-      )}
-      <div className="row">
-        {dogs.map(dog => (
-          <div key={dog.id} className="col-md-4 mb-4">
-            <div className="card">
-              <img src={dog.img} alt={dog.name} className="card-img-top" />
-              <div className="card-body">
-                <h5 className="card-title">{dog.name}</h5>
-                <p className="card-text">Age: {dog.age}</p>
-                <p className="card-text">Breed: {dog.breed}</p>
-                <p className="card-text">Zip Code: {dog.zip_code}</p>
-                <button onClick={() => toggleFavorite(dog.id)}>
-                  {favorites.includes(dog.id) ? '💔 Remove from Favorites' : '❤️ Add to Favorites'}
-                </button>
-              </div>
-            </div>
+      ) : (
+        <>
+          <div className="mb-4">
+            <label htmlFor="breedSelect" className="form-label">Select a Breed</label>
+            <select id="breedSelect" className="form-select" value={selectedBreed} onChange={(e) => setSelectedBreed(e.target.value)}>
+              <option value="">Select a breed...</option>
+              {breeds.map(breed => <option key={breed} value={breed}>{breed}</option>)}
+            </select>
           </div>
-        ))}
-      </div>
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <button onClick={() => handleSearch(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
-        <button onClick={() => handleSearch(currentPage + 1)}>Next</button>
-      </div>
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <button className="btn btn-primary mb-4" onClick={() => handleSearch(1)}>Search</button>
+            <button className="btn btn-secondary mb-4 ml-2" onClick={() => setShowAdvanced(!showAdvanced)}>Advanced Options</button>
+          </div>
+          {showAdvanced && (
+            <div className="mb-4">
+              <label htmlFor="ageMin">Min Age:</label>
+              <input type="number" id="ageMin" value={ageMin} onChange={(e) => setAgeMin(e.target.value)} />
+              <label htmlFor="ageMax">Max Age:</label>
+              <input type="number" id="ageMax" value={ageMax} onChange={(e) => setAgeMax(e.target.value)} />
+              <label htmlFor="zipCode">Zip Code:</label>
+              <input type="text" id="zipCode" value={zipCode} onChange={(e) => setZipCode(e.target.value)} />
+            </div>
+          )}
+          <div className="row">
+            {dogs.map(dog => (
+              <div key={dog.id} className="col-md-4 mb-4">
+                <div className="card">
+                  <img src={dog.img} alt={dog.name} className="card-img-top" />
+                  <div className="card-body">
+                    <h5 className="card-title">{dog.name}</h5>
+                    <p className="card-text">Age: {dog.age}</p>
+                    <p className="card-text">Breed: {dog.breed}</p>
+                    <p className="card-text">Zip Code: {dog.zip_code}</p>
+                    <button onClick={() => toggleFavorite(dog.id)}>
+                      {favorites.includes(dog.id) ? 'Remove from Favorites' : 'Add to Favorites'}
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </animated.div>
   );
 }
